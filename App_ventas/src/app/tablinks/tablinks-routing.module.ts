97b2a7eb3fc1dc80045eb97de56ef504a684dc20
meybel <ma.larenas@duocuc.cl>
2022-09-05 21:29:43 -0300

    enrutamiento arreglado

--- App_ventas/src/app/tablinks/tablinks-routing.module.ts
+++ App_ventas/src/app/tablinks/tablinks-routing.module.ts
@@ -1,31 +1,80 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+
+
 
 import { TablinksPage } from './tablinks.page';
 
-const routes: Routes = [
-  {
-    path:'catalogo',
-    loadChildren: () => import('../catalogo/catalogo.module').then( m => m.CatalogoPageModule)
-  },
-  {
-    path:'inicio',
-    loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path:'perfil',
-    loadChildren: () => import('../perfil/perfil.module').then( m => m.PerfilPageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'inicio',
-    pathMatch: 'full'
-  },
-    
+
+
+const routes: Routes = [{
+
+ path: 'tablinks', 
+
+ component: TablinksPage,
+
+ children: [ 
+
+ {
+
+ path:'catalogo',
+
+ loadChildren: () => import('../catalogo/catalogo.module').then( m => m.CatalogoPageModule)
+
+ },
+
+ {
+
+ path:'inicio',
+
+ loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
+
+ },
+
+ {
+
+ path:'perfil',
+
+ loadChildren: () => import('../perfil/perfil.module').then( m => m.PerfilPageModule)
+
+ },
+
+ {
+
+ path: '',
+
+ redirectTo: 'tablinks/inicio',
+
+ pathMatch: 'full'
+
+ },
+
+ ]
+
+ },
+
+ {
+
+ path: '',
+
+ redirectTo: 'tablinks/inicio',
+
+ pathMatch: 'full'
+
+ },
+
 ];
 
+
+
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+
+ imports: [RouterModule.forChild(routes)],
+
+ exports: [RouterModule],
+
 })
+
 export class TablinksPageRoutingModule {}
+
