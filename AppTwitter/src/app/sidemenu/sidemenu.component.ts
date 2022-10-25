d0e7d13884213c6e0b2a4ad93e0cbae365ce34ab
Jorge Andres Muñoz Muñoz <jorge.munoz.m@munimacul.cl>
2022-10-25 17:12:35 -0300

    Creado sidemenu

---
+++ AppTwitter/src/app/sidemenu/sidemenu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styleUrls: ['./sidemenu.component.scss'],
+})
+export class SidemenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- AppTwitter/src/app/tabs/tabs-routing.module.ts
+++ AppTwitter/src/app/tabs/tabs-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
 
 const routes: Routes = [
   {
@@ -9,28 +10,36 @@ const routes: Routes = [
     children: [
       {
         path: 'tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+        loadChildren: () =>
+          import('../tab1/tab1.module').then((m) => m.Tab1PageModule),
       },
       {
         path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        loadChildren: () =>
+          import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
       },
       {
         path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        loadChildren: () =>
+          import('../tab3/tab3.module').then((m) => m.Tab3PageModule),
       },
       {
         path: '',
         redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      }
-    ]
+        pathMatch: 'full',
+      },
+      {
+        path: 'menu',
+        redirectTo: 'folder/Inbox',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
     redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
